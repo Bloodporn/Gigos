8cc0980f219f1df523f31dae2dd9fd61e155445c
Makunika <48028993+Makunika@users.noreply.github.com>
2019-03-09 01:15:52 +0300

    Initial commit

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+
+    public float maxValue = 100;
+    public Color color = Color.red;
+    public int width = 4;
+    public Slider slider;
+    public bool isRight;
+
+    private static float current;
+
+    void Start()
+    {
+        slider.fillRect.GetComponent<Image>().color = color;
+
+        slider.maxValue = maxValue;
+        slider.minValue = 0;
+        current = maxValue;
+
+    }
+
+    public static float currentValue
+    {
+        get { return current; }
+    }
+
+    void Update()
+    {
+        if (current < 0) current = 0;
+        if (current > maxValue) current = maxValue;
+        slider.value = current;
+    }
+
+    public static void AdjustCurrentValue(float adjust)
+    {
+        current += adjust;
+    }
+}
+
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PressGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void PressExitApp()
+    {
+        Application.Quit();
+        Debug.Log("EXIT");      
+    }
+}
